749a98438e3da9e0195018350ebb4074d8a9998b
rubensmarcondesv8 <rubensmarcondesv8@gmail.com>
2020-06-15 17:36:19 -0300

    Algs aletórios

---
+++ src/algoritmos/instertionSortContrario.java
@@ -0,0 +1,20 @@
+package algoritmos;
+
+import java.util.Arrays;
+
+public class instertionSortContrario {
+	public static void main(String[] args) {
+		int[] arrayTeste = {31, 41, 59, 26, 41, 58};
+		int chave, i;
+		for(int j = 1; j < arrayTeste.length; j++) {
+			chave = arrayTeste[j];
+			i = j - 1;
+			while (i >= 0 && arrayTeste[i] < chave) {
+				arrayTeste[i + 1] = arrayTeste[i];
+				i--;
+			}
+			arrayTeste[i + 1] = chave;
+		}
+		System.out.println(Arrays.toString(arrayTeste));
+	}
+}
---
+++ src/algoritmos/somaDoisBinarios.java
@@ -0,0 +1,11 @@
+package algoritmos;
+
+public class somaDoisBinarios {
+	public static void main(String[] args) {
+		int a, b, c;
+		a = //inteiro binário num arranjo de n bits
+		b = // idem
+		c = //resultado inteiro binário da soma de a + b e deverá ser um arranjo de n+1 bits
+	}
+
+}
